b41915cc496a190eaa22fe7362e017f728aa6999
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-02 09:15:32 -0800

    python import & modules task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -23,5 +23,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1 = tuple_b[0]
         b2 = tuple_b[1]
 
-    new_tuple = (a1 + b1, a2+ b2)
+    new_tuple = (a1 + b1, a2 + b2)
     return new_tuple
