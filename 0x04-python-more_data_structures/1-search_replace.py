1a6caa773aee0586e84963d8c89c22bd16993bae
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-06 09:37:40 -0800

    data structure dictionary & set task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda r : replace if r == search else r, my_list))
+    new_list = list(map(lambda r: replace if r == search else r, my_list))
     return new_list
