1c8a1a055c4e6b8d4883299766bb71c253e62424
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-06 09:30:24 -0800

    data structure dictionary & set task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_mat = matrix.copy()
     for i in range(len(matrix)):
-        new_mat[i] = list(map(lambda x : x**2, matrix[i]))
+        new_mat[i] = list(map(lambda x : x ** 2, matrix[i]))
     return new_mat
