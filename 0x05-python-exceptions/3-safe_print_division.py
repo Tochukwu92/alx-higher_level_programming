745db9f723f20b4233b4b99f72afdb8ae9b70779
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-19 02:57:00 -0800

    python try & except task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, TypeError):
+    except (ZeroDivisionError):
         result = None
     finally:
-        print("Inside result: {:.1f}".format(result))
+        print("Inside result: {}".format(result))
         return result
