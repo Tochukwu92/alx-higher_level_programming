a3152346ce017a1907a819c97d8bccf037e3c83a
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-18 23:58:13 -0800

    python try & except task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    lenght = 0
+    for lists in range(0, x):
+        try:
+            print(my_list[lists], end="")
+            lenght += 1
+        except:
+            break
+    print()
+    return lenght
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+python try exception task
