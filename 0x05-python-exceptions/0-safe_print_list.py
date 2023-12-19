4ff2bb0f63020f9c97f6beb24c9ee69a5b5084d5
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-19 03:14:52 -0800

    python try & except task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[lists], end="")
             lenght += 1
-        except:
+        except IndexError:
             break
     print()
     return lenght
