66abb540d4572eba1ec1bb512bee3073776fcebc
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-19 03:21:52 -0800

    python try & except task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        except(TypeError, ValueError):
-            pass
-            
+        except (ValueError, TypeError):
+            pass 
     print()
     return count
