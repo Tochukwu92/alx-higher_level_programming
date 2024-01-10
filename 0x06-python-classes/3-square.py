53c54f715427e95976d9698c234a105b115d3a1f
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-01-10 06:03:35 -0800

    python class and object

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''Define a class Square. '''
+
+
+class Square:
+    '''Represent a new square. '''
+
+    def __init__(self, size=0):
+        '''Initialize the square.
+
+        Args:
+            size (int): the size of the square
+
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
