74ccab145ee29e09f8d1fee8aaa43bf3ddbcf2e4
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-01-10 05:40:09 -0800

    python class and object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''Define a class Square. '''
+
+
+class Square:
+    '''Represent a new square. '''
+
+    def __init__(self, size):
+        '''Initialize the square.
+
+        Args:
+            size (int): the size of the square
+        
+        '''
+        self.__size = size
