836ab08c0ae3460762504bcaf70a4251255a074a
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-01-16 12:28:15 -0800

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''Define a class.'''
+
+
+class BaseGeometry:
+    '''Introduce a class BaseGeometry.'''
+
+    def area(self):
+        '''Define a function area.'''
+
+        raise Exception("area() is not implemented")
