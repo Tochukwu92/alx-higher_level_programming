98c98db754bec1b94b9f75265fd520011472adf1
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-02-03 02:49:13 -0800

    python file input/output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Opens a text file and prints its content to the standard output.
+
+    Args:
+        filename (str): The name of the file to be read.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        store = f.read()
+        print(store)
