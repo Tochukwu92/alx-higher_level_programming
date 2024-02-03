1dddfbe46f9fea98cc94623a8ed53566f3d0ab92
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-02-03 03:17:11 -0800

    python file input/output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """
+    Writes the given text to the specified file in UTF-8 encoding.
+    Args:
+        filename (str): The name of the file to be written.
+        text (str): The string to be written to the file.
+    Returns:
+        int: The number of characters written to the file.
+    """
+
+    with open(filename, 'w', encoding='UTF8') as f:
+        content = f.write(text)
+    return content
